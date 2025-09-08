be5c9810f1e3639b98125967983e86be088a992c
TamarMaRa <aldubitamara@gmail.com>
2025-09-08 14:22:07 +0300

    part 3

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 name = input(" Hello Enter your  name: ")
 #2
 animal = input(" Hello Enter your an animal: ")
-print(f"my name is {name}, random animal: {animal}")
+print(f"my name is {name}, random animal:{animal}")
 
 
 """
