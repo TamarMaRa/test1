49189ce310bc313e30e88f37f082f3ff41133e6f
TamarMaRa <aldubitamara@gmail.com>
2025-09-08 14:13:45 +0300

    part 2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #1
 name = input(" Hello Enter your  name: ")
 #2
-adress = input(" Hello Enter your adress: ")
-print(f"my name is {name} and I live at {adress}")
+animal = input(" Hello Enter your an animal: ")
+print(f"my name is {name}, random animal: {animal}")
 
 
 """
