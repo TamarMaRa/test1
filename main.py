95d1628fc3ef1c669e647bd369465ab2fcaf693a
TamarMaRa <aldubitamara@gmail.com>
2025-09-08 14:04:11 +0300

    part 1

---
+++ main.py
@@ -0,0 +1,11 @@
+#1
+name = input(" Hello Enter your  name: ")
+#2
+adress = input(" Hello Enter your adress: ")
+print(f"my name is {name} and I live at {adress}")
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
