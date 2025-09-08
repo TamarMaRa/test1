db8a571a7a5ac94afb93f4aba679c6de85402615
TamarMaRa <aldubitamara@gmail.com>
2025-09-08 14:27:27 +0300

    conflict

--- main.py
+++ main.py
@@ -2,10 +2,4 @@
 name = input(" Hello Enter your  name: ")
 #2
 animal = input(" Hello Enter your an animal: ")
-print(f"my name is {name}, random animal:{animal}")
-
-
-"""
-name = "Name" 
-print(" Hello " , name ," How are you:  " )
-"""
+print(f"my name is {name} and I love {animal}")
